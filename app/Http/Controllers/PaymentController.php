0ca616e3b43c0415847b8332993e34d2015f91f7
unknown <you@example.com>
2020-11-24 15:13:33 +0200

    p-25

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -14,22 +14,23 @@ class PaymentController extends Controller
 {
     public function Payment(Request $request)
     {
-        $data = [];
         $data = $request->except(["_token"]);
-        // // dd($data);
-
-        if ($request->payment == 'stripe') {
-            return view('pages.payment.stripe', compact('data'));
-        } elseif ($request->payment == 'paypal') {
-            # code...
-        } elseif ($request->payment == 'ideal') {
-            # code...
+        $view = $request->payment === 'stripe'  ? view('pages.payment.stripe', compact('data'))  : "Cash On Delivery";
+        return $view;
+        if ($request->payment === "paybal") {
+            return " ";
         } else {
-            echo "Cash On Delivery";
+            return " ";
         }
+        // $data = [];
+        // if ($request->payment == 'stripe') return view('pages.payment.stripe', compact('data'));
+        // elseif ($request->payment == 'paypal') return " ";
+        // if ($request->payment != 'stripe' || $request->payment != 'paypal') echo "Cash On Delivery";
     }
     public function buyByStripe(request $request)
     {
+        $email = Auth::user()->email;
+        $total = $request->total;
         // Set your secret key. Remember to switch to your live secret key in production!
         // See your keys here: https://dashboard.stripe.com/account/apikeys
         \Stripe\Stripe::setApiKey('sk_test_XHDYe5qHOaEJPOzTRCJXhD4Q00V435DosZ');
@@ -38,6 +39,7 @@ class PaymentController extends Controller
         // Get the payment token ID submitted by the form:
         $token = $_POST['stripeToken'];
 
+
         $charge = \Stripe\Charge::create([
             'amount' => 999,
             'currency' => 'usd',
@@ -45,6 +47,58 @@ class PaymentController extends Controller
             'source' => $token,
             'metadata' => ['order_id' => uniqid()],
         ]);
-        dd($charge);
+        // dd($charge);
+        $data['user_id'] = Auth::id();
+        $data['payment_id'] = $charge->payment_method;
+        $data['paying_amount'] = $charge->amount;
+        $data['blnc_transection'] = $charge->balance_transaction;
+        $data['stripe_order_id'] = $charge->metadata->order_id;
+        $data['shipping'] = $request->shipping;
+        $data['vat'] = $request->vat;
+        $data['total'] = $request->total;
+        $data['payment_type'] = $request->payment_type;
+        $data['status_code'] = mt_rand(100000, 999999);
+        if (Session::has('coupon')) {
+            $data['subtotal'] = Session::get('coupon')['balance'];
+        } else {
+            $data['subtotal'] = Cart::Subtotal();
+        }
+        $data['status'] = 0;
+        $data['date'] = date('d-m-y');
+        $data['month'] = date('F');
+        $data['year'] = date('Y');
+        $order_id = DB::table('orders')->insertGetId($data);
+
+        $shipping['order_id'] = $order_id;
+        $shipping['ship_name'] = $request->ship_name;
+        $shipping['ship_phone'] = $request->ship_phone;
+        $shipping['ship_email'] = $request->ship_email;
+        $shipping['ship_address'] = $request->ship_address;
+        $shipping['ship_city'] = $request->ship_city;
+        DB::table('shippings')->insert($shipping);
+
+        // Insert Order Details Table
+
+        $content = Cart::content();
+        $details = array();
+        foreach ($content as $row) {
+            $details['order_id'] = $order_id;
+            $details['product_id'] = $row->id;
+            $details['product_name'] = $row->name;
+            $details['color'] = $row->options->color;
+            $details['size'] = $row->options->size;
+            $details['quantity'] = $row->qty;
+            $details['singleprice'] = $row->price;
+            $details['totalprice'] = $row->qty * $row->price;
+            DB::table('orders_details')->insert($details);
+        }
+
+        Cart::destroy();
+        if (Session::has('coupon')) Session::forget('coupon');
+        $notification = array(
+            'messege' => 'Order Process Successfully Done',
+            'alert-type' => 'success'
+        );
+        return Redirect()->to('/')->with($notification);
     }
 }
